e8b2ca27078495815c7f0f694631f077e0dc6c14
maskarabuya <118160062+maskarabuya@users.noreply.github.com>
2022-11-18 19:47:08 +0200

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,23 @@
+#include  <string>
+#include  <iostream>
+
+using namespace std;
+
+int main() {
+
+    long long dteu = 10;
+
+    dteu += 7;
+
+
+    bool good = true;
+    bool ne_good = false;
+
+    if (dteu == 17)
+        cout << good,"DTEU is GOD of Learning";
+    else {
+        cout << ne_good, "Try again";
+    }
+
+
+}
